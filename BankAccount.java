cc9e78fc84dd0588c5f9879db7c9af636b772d8c
Tami Takada <tami@mytalos.com>
2020-10-27 09:13:51 -0400

    adding BankAccount.java and constructor

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public static void main(String args[]) {
+
+  }
+
+  public BankAccount(int a, String p) {
+    balance = 0;
+    accountID = a;
+    password = p;
+  }
+
+}
